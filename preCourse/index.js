fa133beb9dfb3d5a263cb4681f88dccb4f63239b
tony <tonysorensen4@gmail.com>
2020-03-16 17:17:30 -0700

    Lambda School Precourse Website

---
+++ preCourse/index.js
@@ -0,0 +1,11 @@
+// define the function to operate your button here 
+
+var button = document.getElementById("redirect");
+
+button.onmouseover = function () {
+    this.style.backgroundColor = "#2EB2D7 ";
+}
+
+button.onmouseout = function () {
+    this.style.backgroundColor = "#117A97  ";
+}
